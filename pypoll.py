47b18c6daf088280b31b86a2c1f7fd7f9d59a542
Brett-Huseman <71539029+Brett-Huseman@users.noreply.github.com>
2020-10-16 00:27:53 -0500

    Add files via upload

---
+++ pypoll.py
@@ -0,0 +1,7 @@
+#Import Data from CSV File
+#1. Total Number of Votes Cast
+#2. A List of Canidates who Recieved Votes
+#3. The Percentage of votes each canidate won
+#4. The Total Number of Votes each canidate won
+#5. The winner of the election based on popular vote
+#Print the results
